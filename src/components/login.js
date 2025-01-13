cf5b274818e55a020434e36724770cb7915d2647
platan12 <150144929+platan12@users.noreply.github.com>
2025-01-13 10:33:40 +0100

    autointification

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login.css';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    // Aquí podries afegir validacions abans de redirigir
-    navigate('/DatabaseSpot'); // Navega a la pàgina "Dashboard"
+    if (username === 'admin' && password === 'admin') {
+      // Si l'usuari i contrasenya són correctes, redirigeix a /dashboard
+      navigate('/DatabaseSpot');
+    } else {
+      // Si són incorrectes, mostra un missatge d'error
+      setErrorMessage('Usuari o contrasenya incorrectes');
+    }
   };
 
   return (
@@ -18,9 +26,20 @@ const Login = () => {
       </header>
       <div className="login-form">
         <img src="/logo.png" alt="Logo" className="logo2" />
-        <input type="text" placeholder="Usuari" />
-        <input type="password" placeholder="Contrasenya" />
+        <input
+          type="text"
+          placeholder="Usuari"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} // Actualitza el valor de l'usuari
+        />
+        <input
+          type="password"
+          placeholder="Contrasenya"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} // Actualitza el valor de la contrasenya
+        />
         <button onClick={handleLogin}>Log In</button>
+        {errorMessage && <p className="error-message">{errorMessage}</p>} {/* Mostra el missatge d'error */}
       </div>
     </div>
   );
